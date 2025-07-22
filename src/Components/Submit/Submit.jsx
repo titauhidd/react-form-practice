7c57c002680d7bd1fe3e3c750b8b2f7b13047a9b
titauhidd <tauhidultonmoy1234@gmail.com>
2025-07-22 23:24:41 +0600

    practiced error

--- src/Components/Submit/Submit.jsx
+++ src/Components/Submit/Submit.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Submit = () => {
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e)
-    console.log("form submitted");
+    console.log(e);
+    // console.log("form submitted");
+    if (password.length < 8) {
+      setError("Erorr");
+    } else {
+      setError("");
+      console.log(password);
+    }
+  };
+
+  const handlePassword = (e) => {
+    console.log(e);
+    console.log(e.target.value);
+    setPassword(e.target.value);
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" />
         <br />
+        <input onChange={handlePassword} type="password" name="password" />
+        <br />
         <input type="submit" name="" id="" value="submit" />
+        {error ? `${error} Fuck you` : ""}
       </form>
     </div>
   );
